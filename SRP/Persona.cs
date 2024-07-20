2b486f3f6868d24561b5ab9961a745be698a5b10
Nicole Tremols Samboy <20221764@itla.edu.do>
2024-07-20 15:14:26 -0400

    Add project files.

---
+++ SRP/Persona.cs
@@ -0,0 +1,25 @@
+﻿namespace SRP
+{
+    public class Persona
+    {
+        private string? nombre;
+        private int? edad;
+        private string? direccion;
+        private string? correoElectronico;
+    }
+    
+     public class CrearDatos
+     {
+         Console.WriteLine($"Nombre: {nombre}");
+         Console.WriteLine($"Edad: {edad}");
+         Console.WriteLine($"Direccion: {direccion}");
+         Console.WriteLine($"Correo Electronico: {correoElectronico}");
+     }
+    
+
+    public class EnviarCorreoElectronico(string correoElectronico, string mensaje)
+    {
+        //logica para enviar correo electronico 
+    }
+}
+    
